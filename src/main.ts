baf12eb9876c93cf1c66637be6f217dcb5635701
Melek Redwan <melekredwan@icloud.com>
2025-05-22 19:21:58 -0600

    feat: parse file into JSON rows

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,21 @@ const $btn = document.querySelector('#convert-btn') as HTMLButtonElement;
 let xmlString = "";
 
 $file.addEventListener('change', async () => {
+    // grab file
     const file = $file.files?.[0];
     if (!file) return; 
-
+    
     $log.textContent = `Loading: ${file.name} ...`;
+    
+    // parse loaded file 
     const buffer = await file.arrayBuffer();
-    $log.textContent = `Loaded`;
+    const wb = read(buffer, {type: 'array'});
+    const sheet = wb.Sheets[wb.SheetNames[0]]; // grab the first sheet
+    const rows = utils.sheet_to_json<Record<string, string>>( // converting to JSON
+        sheet, { raw: false }
+    );
+
+    console.log(rows);
+    
+    $log.textContent = `Parsed ${rows.length} rows - see console`
 });
